a1315e6371013f9001aa1d6943f07860cd7d9bb7
Arvind K <arvind.ku.akash@gmail.com>
2022-02-09 03:30:43 -0700

    Time: 36 ms (55.96%), Space: 13.9 MB (94.46%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.py
+++ 118-pascals-triangle/118-pascals-triangle.py
@@ -9,9 +9,9 @@ class Solution:
         
         for i in range(2, n):
             l = [1]
-            t = arr[i-1]
-            for j in range(len(t)-1):
-                l.append(t[j]+t[j+1])
+            # t = arr[i-1]
+            for j in range(len(arr[i-1])-1):
+                l.append(arr[i-1][j]+arr[i-1][j+1])
             l.append(1)
             arr.append(l)
             
