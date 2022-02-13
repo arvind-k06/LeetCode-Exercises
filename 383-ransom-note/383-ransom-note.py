60502beac0dac93aa8778db59410fafc937b24fa
Arvind K <arvind.ku.akash@gmail.com>
2022-02-12 21:30:41 -0700

    Time: 80 ms (51.95%), Space: 14.1 MB (98.25%) - LeetHub

--- 383-ransom-note/383-ransom-note.py
+++ 383-ransom-note/383-ransom-note.py
@@ -1,15 +1,24 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        r = ransomNote
-        m = magazine
         
-        for i in range(len(r)):
-            if r[i] in m:
-                b = m.index(r[i])
-                m = m[0:b]+m[b+1:]
-            
-            else:
-                return False
-            
+        r = set(ransomNote)
+        
+        for i in r:
+            if ransomNote.count(i)>magazine.count(i): return False
         return True
+        
+        
+        
+#         r = ransomNote
+#         m = magazine
+        
+#         for i in range(len(r)):
+#             if r[i] in m:
+#                 b = m.index(r[i])
+#                 m = m[0:b]+m[b+1:]
+            
+#             else:
+#                 return False
+            
+#         return True
         
