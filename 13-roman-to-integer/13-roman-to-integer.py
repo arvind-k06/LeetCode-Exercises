6b6ce36c26e7f481c7072c45d42314be9780f477
Arvind K <arvind.ku.akash@gmail.com>
2022-04-02 13:04:53 -0700

    Time: 113 ms (5.17%), Space: 13.9 MB (34.02%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.py
+++ 13-roman-to-integer/13-roman-to-integer.py
@@ -1,18 +1,18 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        translations = {
-            "I": 1,
-            "V": 5,
-            "X": 10,
-            "L": 50,
-            "C": 100,
-            "D": 500,
-            "M": 1000
-        }
-        number = 0
-        s = s.replace("IV", "IIII").replace("IX", "VIIII").replace("CD", "CCCC").replace("CM", "DCCCC").replace("XL", "XXXX").replace("XC", "LXXXX")
-
-        for char in s:
-            number += translations[char]
-        return number
+        table1 = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+        table2 = {"IV": 4, "IX": 9, "XL": 40, "XC": 90, "CD": 400, "CM": 900}
+        c = 0
+        val = 0
+        while c < len(s):
+            
+            if s[c:c+2] in table2:
+                val += table2[s[c:c+2]]
+                c += 2
+                
+            elif s[c] in table1:
+                val += table1[s[c]]
+                c += 1
+                
+        return val
         
