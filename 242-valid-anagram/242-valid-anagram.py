1459535bc1c0a52174790b3b48c9099c8c33af5b
Arvind K <arvind.ku.akash@gmail.com>
2022-04-02 13:15:29 -0700

    Time: 54 ms (76.95%), Space: 14.4 MB (69.75%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -1,20 +1,6 @@
+from collections import Counter
+
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        
-#         if len(s)!=len(t): return False
-        
-#         if set(s)!=set(t): return False
-        
-        for i in range(len(s)):
-            if s[i] in t:
-                t = t.replace(s[i], "", 1)
-            else:
-                return False
-                # print(t)
-            
-        # print(t)
-        if t=="": return True
-            
-        
-        return False
+        return Counter(s) == Counter(t)
         
