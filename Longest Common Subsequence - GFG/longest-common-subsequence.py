f3f4934f61a8316f56aca18627c923bec6b54e10
Arvind K <arvind.ku.akash@gmail.com>
2022-04-02 16:37:22 -0700

    Added solution - LeetHub

---
+++ Longest Common Subsequence - GFG/longest-common-subsequence.py
@@ -0,0 +1,41 @@
+#User function Template for python3
+
+class Solution:
+    
+    #Function to find the length of longest common subsequence in two strings.
+    def lcs(self,x,y,s1,s2):
+        
+        dp = [[0]* (y+1) for _ in range(x+1)]
+        
+        for i in range(x+1):
+            for j in range(y+1):
+                if i == 0 or j == 0:
+                    dp[i][j] = 0
+                elif s1[i-1] == s2[j-1]:
+                    dp[i][j] = dp[i-1][j-1]+1
+                else:
+                    dp[i][j] = max(dp[i][j-1], dp[i-1][j])
+                    
+        
+        return dp[-1][-1]
+        
+        # code here
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+import atexit
+import io
+import sys
+
+# Contributed by : Nagendra Jha
+
+if __name__ == '__main__':
+    test_cases = int(input())
+    for cases in range(test_cases):
+        x,y = map(int,input().strip().split())
+        s1 = str(input())
+        s2 = str(input())
+        ob=Solution()
+        print(ob.lcs(x,y,s1,s2))
+# } Driver Code Ends
