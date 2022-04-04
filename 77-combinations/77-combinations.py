5996b99a09ec3716d5c187a23a0fd295e1999d9b
Arvind K <arvind.ku.akash@gmail.com>
2022-04-04 02:18:26 -0700

    Time: 134 ms (84.81%), Space: 16.1 MB (20.20%) - LeetHub

---
+++ 77-combinations/77-combinations.py
@@ -0,0 +1,11 @@
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        if k == 0:
+            return [[]]
+        res = []
+        for i in range(k, n+1):
+            for pre in self.combine(i-1, k-1):
+                res.append(pre + [i])
+                
+        return res
+        
