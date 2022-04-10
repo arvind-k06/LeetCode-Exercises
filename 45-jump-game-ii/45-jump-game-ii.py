aaae90a94f1054b984b14302dc87106e61dae843
Arvind K <arvind.ku.akash@gmail.com>
2022-04-10 16:58:56 -0700

    Time: 148 ms (81.34%), Space: 15 MB (61.76%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.py
+++ 45-jump-game-ii/45-jump-game-ii.py
@@ -12,16 +12,16 @@ class Solution:
                 far=curr
         return res
             
-        memo={}
-        def min_jumps(index):
-            if index>=len(nums)-1:
-                return 0
-            if index in memo:
-                return memo[index]
-            ans=float('inf')
-            for i in range(index,index+nums[index]):
-                ans=min(ans,1+min_jumps(i+1))
-            memo[index]=ans
-            return ans
-        return min_jumps(0)
+#         memo={}
+#         def min_jumps(index):
+#             if index>=len(nums)-1:
+#                 return 0
+#             if index in memo:
+#                 return memo[index]
+#             ans=float('inf')
+#             for i in range(index,index+nums[index]):
+#                 ans=min(ans,1+min_jumps(i+1))
+#             memo[index]=ans
+#             return ans
+#         return min_jumps(0)
         
